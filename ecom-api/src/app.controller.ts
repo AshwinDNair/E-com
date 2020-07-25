b1fef974bad04212b0155452e39e1b7ad7d3cc5f
Ashwin Dinesh <ashwin.dinesh@speridian.com>
2020-07-25 18:03:47 +0530

    Api authentication

--- ecom-api/src/app.controller.ts
+++ ecom-api/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Request, UseGuards, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -10,5 +12,13 @@ export class AppController {
     
     console.log("app");
     return this.appService.getHello();
+  }
+
+  //@UseGuards(AuthGuard('local'))
+@UseGuards(LocalAuthGuard)
+  
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
--- ecom-api/src/app.module.ts
+++ ecom-api/src/app.module.ts
@@ -7,9 +7,11 @@ import { CartsService } from './services/carts.services';
 import { CartsController } from './carts/carts.controller';
 import { OrdersService } from './services/orders.service';
 import { OrdersController } from './orders/orders.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, ItemsController, CartsController,OrdersController],
   providers: [AppService,ItemsService,CartsService,OrdersService],
 })
